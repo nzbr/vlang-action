ef2b74a0c0c027064e642c4814e9ecee7ccb0ab3
nzbr <mail@nzbr.de>
2019-10-28 13:09:58 +0100

    Don't recomile when compiler version is master

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-set -euxo pipefail
+set -ex
+
+whoami
+id -u
 
 if [ "$INPUT_COMPILER" != "master" ]; then
-    pushd /opt/v
-    git clean -xf
-    git fetch
-    git reset --hard origin/master
-    git checkout "$INPUT_COMPILER"
+	pushd /opt/v
+	git clean -xf
+	git fetch
+	git reset --hard origin/master
+	git checkout "$INPUT_COMPILER"
 
-    make
-    v -o v v.v
-    sudo v symlink
-    popd
-else
-    v up
+	make
+	v -o v v.v
+	sudo v symlink
+	popd
 fi
 
 v $INPUT_ARGS $INPUT_FILE
